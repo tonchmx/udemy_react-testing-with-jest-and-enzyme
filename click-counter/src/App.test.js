17315d419f8204b27a9ff180956441bddb309222
Toño Cruz <tonchmx@gmail.com>
2021-05-26 13:59:44 -0500

    Started testing

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('[data-test="component-app"]');
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+
+});
+
+test('renders counter display', () => {
+
+});
+
+test('counter display starts at 0', () => {
+
+});
+
+test('clicking button increments counter', () => {
+
 });
