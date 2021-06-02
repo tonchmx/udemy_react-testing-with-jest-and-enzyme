686cbc4bd7c0a3a9add9a5b07205863836ab2678
Toño Cruz <tonchmx@gmail.com>
2021-06-02 12:19:28 -0500

    Add proptypes for Congrats component

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Congrats = (props) => {
     if (props.success) {
@@ -14,6 +15,10 @@ const Congrats = (props) => {
             <div data-test="component-congrats" />
         )
     }
+}
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired
 }
 
 export default Congrats
