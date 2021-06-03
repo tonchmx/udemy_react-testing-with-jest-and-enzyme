b80a4a88e0554edf98f5529292de8bd58b781bb2
Toño Cruz <tonchmx@gmail.com>
2021-06-03 13:23:58 -0500

    Added props and checkprops test to input component

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { findByTestAttr } from '../test/testUtils'
+import { findByTestAttr, checkProps } from '../test/testUtils'
 
 import Input from './Input'
 
-const setup = () => {
-    return shallow(<Input />)
+const setup = (secretWord = 'party') => {
+    return shallow(<Input secretWord={secretWord} />)
 }
 
 test('renders without errors', () => {
     const wrapper = setup()
     const component = findByTestAttr(wrapper, 'component-input')
     expect(component.length).toBe(1)
+})
+
+test('does not throw warning with expected props', () => {
+    checkProps(Input, { secretWord: 'party' })
 })
