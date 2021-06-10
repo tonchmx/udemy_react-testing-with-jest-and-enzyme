9d4eb60d387e59d9675b97e8054cb133bb61129e
Toño Cruz <tonchmx@gmail.com>
2021-06-10 12:01:07 -0500

    Basis for app component

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -4,7 +4,7 @@ import GuessedWords from './GuessedWords'
 
 function App() {
   return (
-    <div className="container">
+    <div className="container" data-test="component-app">
       <h1>Jotto</h1>
       <Congrats success={true} />
       <GuessedWords guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]} />
