ea251a1991c1b628c78ab97c5317799063264233
Toño Cruz <tonchmx@gmail.com>
2021-06-17 12:01:36 -0500

    Add functional tests for guess word

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Congrats from './Congrats'
 import GuessedWords from './GuessedWords'
+import Input from './Input'
 
 function App() {
+  // TODO: get props from shared state
+  const success = false
+  const secretWord = 'party'
+
   return (
     <div className="container" data-test="component-app">
       <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]} />
+      <Congrats success={success} />
+      <Input success={success} secretWord={secretWord} />
+      <GuessedWords guessedWords={[]} />
     </div>
   );
 }
