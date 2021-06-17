2a57b5e0365c2b98984345d457796eb8fcb48cfc
Toño Cruz <tonchmx@gmail.com>
2021-06-17 12:57:26 -0500

    Mocked actions to use in App tests

--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,9 +1,13 @@
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { findByTestAttr } from '../test/testUtils'
 import App from './App';
 
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions')
+import { getSecretWord as mockGetSecretWord } from './actions'
+
 const setup = () => {
-    return shallow(<App />)
+    return mount(<App />)
 }
 
 test('renders without error', () => {
@@ -11,3 +15,25 @@ test('renders without error', () => {
     const appComponent = findByTestAttr(wrapper, 'component-app')
     expect(appComponent).toHaveLength(1)
 });
+
+describe('get secret word', () => {
+    beforeEach(() => {
+        // clear the mock calls from previous tests
+        mockGetSecretWord.mockClear()
+    })
+
+    test('getSecreWord on app mount', () => {
+        setup()
+
+        expect(mockGetSecretWord).toHaveBeenCalledTimes(1)
+    })
+
+    test('getSecreWord does not run on app update', () => {
+        const wrapper = setup()
+        mockGetSecretWord.mockClear()
+
+        wrapper.setProps()
+
+        expect(mockGetSecretWord).toHaveBeenCalledTimes(0)
+    })
+})
